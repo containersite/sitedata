8b4ee60390d3449d0f1376ea4823776d9d7f729f
containersite <sikdarparves527@gmail.com>
2025-11-18 13:11:27 +0600

    Update one.js

--- site-one/one.js
+++ site-one/one.js
@@ -1,7 +1,3 @@
-// URLs - এখানে লিঙ্ক আলাদা রাখা হয়েছে
-const watchURL = "https://bitly.cx/VjZJ";
-const downloadURL = "https://bitly.cx/VjZJ";
-const redirectURL = "https://bitly.cx/nW87";
 
 // Elements
 const progressFill = document.getElementById('progress-fill');
